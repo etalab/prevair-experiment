de202f2d0407e40ffa7d37dcb79c709e4b920c67
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2018-02-23 22:45:35 +0100

    Support all measures + prebuild data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const {join} = require('path')
 const {parse} = require('querystring')
 const {send} = require('micro')
 const {createSpatialIndex} = require('./lib/spatial')
+const {records} = require('./prebuilt-data.json')
 
 /* Micro helpers */
 
@@ -15,11 +15,11 @@ function badRequest(res, message) {
   return send(res, 400, {code: 400, message})
 }
 
-const spatialIndex = createSpatialIndex(join(__dirname, 'data', 'PREVAIR.prevision.20180223.MAXJ0.PM25.public.nc'))
+const spatialIndex = createSpatialIndex(records)
 
 /* API */
 
-module.exports = async (req, res) => {
+module.exports = (req, res) => {
   const qs = extractQueryString(req)
   if (!qs.lat || !qs.lon) return badRequest(res, 'Les paramètres lat et lon sont obligatoires')
   const lat = Number.parseFloat(qs.lat)
@@ -27,7 +27,7 @@ module.exports = async (req, res) => {
   if (Number.isNaN(lat) || Number.isNaN(lon) || lat >= 90 || lat <= -90 || lon >= 180 || lon <= -180) {
     return badRequest(res, 'Les paramètres lat et lon doivent être des coordonnées WGS-84 valides')
   }
-  const result = await spatialIndex.search({lon, lat})
+  const result = spatialIndex.search({lon, lat})
   if (!result) return badRequest(res, 'Seule la France métropolitaine est couverte par cette API')
   return result
 }
