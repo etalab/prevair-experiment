38307aefa2768b1e1fc5f03eb1e00b0771d55fdc
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2018-02-23 20:55:31 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const {join} = require('path')
+const {parse} = require('querystring')
+const {send} = require('micro')
+const {createSpatialIndex} = require('./lib/spatial')
+
+/* Micro helpers */
+
+function extractQueryString(req) {
+  const pos = req.url.indexOf('?')
+  const qs = pos === -1 ? '' : req.url.substr(pos + 1)
+  return parse(qs)
+}
+
+function badRequest(res, message) {
+  return send(res, 400, {code: 400, message})
+}
+
+const spatialIndex = createSpatialIndex(join(__dirname, 'data', 'PREVAIR.prevision.20180223.MAXJ0.PM25.public.nc'))
+
+/* API */
+
+module.exports = async (req, res) => {
+  const qs = extractQueryString(req)
+  if (!qs.lat || !qs.lon) return badRequest(res, 'Les paramètres lat et lon sont obligatoires')
+  const lat = Number.parseFloat(qs.lat)
+  const lon = Number.parseFloat(qs.lon)
+  if (Number.isNaN(lat) || Number.isNaN(lon) || lat >= 90 || lat <= -90 || lon >= 180 || lon <= -180) {
+    return badRequest(res, 'Les paramètres lat et lon doivent être des coordonnées WGS-84 valides')
+  }
+  const result = await spatialIndex.search({lon, lat})
+  if (!result) return badRequest(res, 'Seule la France métropolitaine est couverte par cette API')
+  return result
+}
